ff29fecf6a98aec31b0c520e273411a13f9be03a
Liam Brophy <liambrophy.design@gmail.com>
2025-01-06 09:48:22 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() + 1);
+
+let num3 = 8;
+let num4 = 2;
+
+let mod = num3 / num4;
+
+let max = Math.max(1, 2, 13, 4, 20);
